aa3adf0dbc57e6fee8ab63c62f81fbd40c649fab
devin t brown <toel3my@gmail.com>
2017-12-02 04:22:13 -0800

    swapped scores[ ][ ] if , else assignment 0 , 1

--- ecproject.py
+++ ecproject.py
@@ -9,7 +9,6 @@ def listgame( A):
             x = scores[i + 2][j] if ( (i + 2) <= j) else 0
             y = scores[i + 1][j - 1] if ((i +1) <= (j-1)) else 0
             z = scores[i][j - 2] if (i <= (j - 2)) else 0
-            print("x:",x,"y:",y,"z:",z)
             scores[i][j] = max( A[i] + min(x,y), A[j] + min(y,z))
             i += 1
     return scores
@@ -26,32 +25,32 @@ def listgameb( A):
                 x = scores[0][j] if ( (i + 2) <= j) else 0
                 y = scores[0][j-1] if ((i +1) <= (j-1)) else 0
                 z = scores[0][j-2] if (i <= (j - 2)) else 0
-                print("x:",x,"y:",y,"z:",z)
-                scores[1][j] = max( A[i] + min(x,y), A[j] + min(y,z))
+                scores[0][j] = max( A[i] + min(x,y), A[j] + min(y,z))
             else:
                 x = scores[1][j] if ( (i + 2) <= j) else 0
                 y = scores[1][j-1] if ((i +1) <= (j-1)) else 0
                 z = scores[1][j-2] if (i <= (j - 2)) else 0
-                print("x:",x,"y:",y,"z:",z)
-                scores[0][j] = max( A[i] + min(x,y), A[j] + min(y,z))                
+                scores[1][j] = max( A[i] + min(x,y), A[j] + min(y,z))                
             i += 1
     return scores
 
 def main():
-    arr = [8,15,3,7]
+    #arr = [8,15,3,7]
+    arr = [20, 30, 2, 2, 2, 10]
     result = listgameb(arr)
     result1 = listgame(arr)
-    
-    print(result)
-    
-    n = len(result)
-    for i in range(n):
+    print(result,"##", result1)
+    n = len(result[0])
+    m = len(result1)
+    for i in range(2):
         for j in range(n):
             print( result[i][j], " ", end="")
         print("")
     print("##########")
-    for i in range(n):
-        for j in range(n):
+
+    for i in range(m):
+        for j in range(m):
             print( result1[i][j], " ", end="")
         print("")
+
 main()
